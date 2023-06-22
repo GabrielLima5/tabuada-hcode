c8ac1473fdd56eb390c8be7dfe396cc1f91112d3
Gabriel Lima <gabriellpbiel@hotmail.com>
2023-06-22 17:26:58 -0300

    melhorando o design da tabuada

--- assets/js/tabuada.js
+++ assets/js/tabuada.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* 
 Exemplo de uma tabuada
 */
@@ -46,53 +45,4 @@ function calculaTabuada(){
 calculaTabuada();
 
 // Adicionando o evento de alteração ao campo número
-=======
-/* 
-Exemplo de uma tabuada
-*/
-
-// Declaração de uma função
-function calculaTabuada(){
-
-    // Obtendo o DOM do tbody da tabela
-    let tabuada = document.querySelector('#tabuada tbody');
-
-    // Obtendo o valor do campo input e convertendo-o em númerico
-    let valorA = parseInt(document.querySelector('#valorA').value);
-
-        // Limpando o conteúdo do tbody
-        tabuada.innerHTML = '';
-
-        // Criando um laço de repetição do 0 ao 10
-    for (let valorB = 0; valorB <= 10; valorB++){
-
-        // Calculando o resultado da linha atual
-        let resultado = valorA * valorB;
-
-        // Criando o template das colunas da linha atual
-        let template = `<td>${valorA}</td>
-                        <td>x</td>
-                        <td>${valorB}</td>
-                        <td>=</td>
-                        <td>${resultado}</td>
-        `;
-
-        // Criando o elemento tr
-        let tr = document.createElement('tr');
-
-        // Inserindo as colunas na linha
-        tr.innerHTML = template;
-
-        // Inserindo a linha na tabela
-        tabuada.append(tr)
-
-    }// Fechando o for
-
-} // Fechando a função
-
-// Chamando a nossa função pela primeira vez
-calculaTabuada();
-
-// Adicionando o evento de alteração ao campo número
->>>>>>> 0ce79520c230f75ee69d85bada7a8f9958cdd4c6
 document.querySelector('#valorA').addEventListener('change', calculaTabuada);
